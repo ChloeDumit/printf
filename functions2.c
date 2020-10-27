845d1f179f14770cbf1d010d73403d87608d629e
Chloe <2106@holbertonschool.com>
2020-10-27 18:26:10 +0000

    betty fix

--- functions2.c
+++ functions2.c
@@ -16,9 +16,10 @@ int rot13(va_list list)
 	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char *str;
+
 	str = va_arg(list, char*);
 
-	if(str == NULL)
+	if (str == NULL)
 		return (-1);
 
 	for (i = 0; str[i] != '\0'; i++)
@@ -31,10 +32,34 @@ int rot13(va_list list)
 				break;
 			}
 		}
-		if(j == 53)
+		if (j == 53)
 		{
 			_put_char(str[i]);
 		}
 	}
 	return (i);
+}
+/**
+ *reverse - prints reverse string
+ *@list: list
+ *Return: list
+ */
+int reverse(va_list list)
+{
+	int count, c;
+	char l;
+	char *s = va_arg(list, char*);
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+	}
+	count--;
+	for (c = 0; c < count; c++)
+	{
+		l = s[count];
+		s[count] = s[c];
+		s[c] = l;
+		count--;
+	}
+	return (count);
 }
